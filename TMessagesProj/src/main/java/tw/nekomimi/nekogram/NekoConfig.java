9ddd80ff781a029d454b9aa32af8efb8c63b9c10
gsjsyjhf <amiramir200422a@gmail.com>
2026-07-27 23:39:41 +0300

    MeeroX v36: iOS bubble tail (quad curves), iOS code entry screen

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -94,6 +94,7 @@ public class NekoConfig {
     public static ConfigItem meeroIosSounds = addConfig("meeroIosSounds", configTypeBool, true);
     public static ConfigItem meeroGlassBorders = addConfig("meeroGlassBorders", configTypeBool, true);
     public static ConfigItem meeroIosLoading = addConfig("meeroIosLoading", configTypeBool, true);
+    public static ConfigItem meeroIosCode = addConfig("meeroIosCode", configTypeBool, true);
 
     // From NekoConfig
     public static ConfigItem useIPv6 = addConfig("IPv6", configTypeBool, false);
