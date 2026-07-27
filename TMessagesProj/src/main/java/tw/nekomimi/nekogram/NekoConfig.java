60928daea56c53011799b044e7e4f7e0586f98d3
gsjsyjhf <amiramir200422a@gmail.com>
2026-07-27 12:11:12 +0300

    MeeroX: glass borders, revert input bar, fix bio band + back arrow

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -91,8 +91,8 @@ public class NekoConfig {
     public static ConfigItem meeroCards = addConfig("meeroCards", configTypeBool, true);
     public static ConfigItem meeroDialogsStyle = addConfig("meeroDialogsStyle", configTypeBool, true);
     public static ConfigItem meeroIosIcons = addConfig("meeroIosIcons", configTypeBool, true);
-    public static ConfigItem meeroIosInput = addConfig("meeroIosInput", configTypeBool, true);
     public static ConfigItem meeroIosSounds = addConfig("meeroIosSounds", configTypeBool, true);
+    public static ConfigItem meeroGlassBorders = addConfig("meeroGlassBorders", configTypeBool, true);
 
     // From NekoConfig
     public static ConfigItem useIPv6 = addConfig("IPv6", configTypeBool, false);
