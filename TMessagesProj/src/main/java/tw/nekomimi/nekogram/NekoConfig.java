3ac05f35b142138bf7c58c9452bd726f11e6fcc3
gsjsyjhf <amiramir200422a@gmail.com>
2026-07-27 05:06:19 +0300

    MeeroX: iOS icons + layout fixes

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -91,6 +91,7 @@ public class NekoConfig {
     public static ConfigItem meeroCards = addConfig("meeroCards", configTypeBool, true);
     public static ConfigItem meeroDialogsStyle = addConfig("meeroDialogsStyle", configTypeBool, true);
     public static ConfigItem meeroIosIcons = addConfig("meeroIosIcons", configTypeBool, true);
+    public static ConfigItem meeroIosInput = addConfig("meeroIosInput", configTypeBool, true);
 
     // From NekoConfig
     public static ConfigItem useIPv6 = addConfig("IPv6", configTypeBool, false);
