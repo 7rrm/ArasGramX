58639cbd52b4a62a8de777831426e237d6017db0
gsjsyjhf <amiramir200422a@gmail.com>
2026-07-28 03:07:47 +0300

    MeeroX v41: iOS index bar, hide search pill on search, revert bottom action bar

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoChatSettingsActivity.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoChatSettingsActivity.java
@@ -105,7 +105,7 @@ public class NekoChatSettingsActivity extends BaseNekoXSettingsActivity implemen
     private final AbstractConfigCell meeroIosCodeRow = cellGroup.appendCell(new ConfigCellTextCheck(NekoConfig.meeroIosCode, getString(R.string.MeeroIosCodeInfo)));
     private final AbstractConfigCell meeroIosIntroRow = cellGroup.appendCell(new ConfigCellTextCheck(NekoConfig.meeroIosIntro, getString(R.string.MeeroIosIntroInfo)));
     private final AbstractConfigCell meeroIosSearchRow = cellGroup.appendCell(new ConfigCellTextCheck(NekoConfig.meeroIosSearch, getString(R.string.MeeroIosSearchInfo)));
-    private final AbstractConfigCell meeroBottomActionsRow = cellGroup.appendCell(new ConfigCellTextCheck(NekoConfig.meeroBottomActions, getString(R.string.MeeroBottomActionsInfo)));
+    private final AbstractConfigCell meeroIosFastScrollRow = cellGroup.appendCell(new ConfigCellTextCheck(NekoConfig.meeroIosFastScroll, getString(R.string.MeeroIosFastScrollInfo)));
     private final AbstractConfigCell dividerMeero = cellGroup.appendCell(new ConfigCellDivider());
 
     // Sticker Size
