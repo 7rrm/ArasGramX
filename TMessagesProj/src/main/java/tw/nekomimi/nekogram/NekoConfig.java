d3f9d231caf63ca98b37d5b54c21d048a2d8d5a0
gsjsyjhf <amiramir200422a@gmail.com>
2026-07-29 04:14:11 +0300

    MeeroX v55: iOS waveform pitch and rounded caps, lighter record indicator

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -102,6 +102,7 @@ public class NekoConfig {
     public static ConfigItem meeroIosHaptics = addConfig("meeroIosHaptics", configTypeBool, true);
     public static ConfigItem meeroIosMenuAnim = addConfig("meeroIosMenuAnim", configTypeBool, true);
     public static ConfigItem meeroIosInputPill = addConfig("meeroIosInputPill", configTypeBool, true);
+    public static ConfigItem meeroIosWaveform = addConfig("meeroIosWaveform", configTypeBool, true);
 
     // From NekoConfig
     public static ConfigItem useIPv6 = addConfig("IPv6", configTypeBool, false);
